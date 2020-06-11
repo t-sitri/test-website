c4240bb9f6cd817f776ea5345ded85cc731c5b85
Sidhika Tripathee <t-sitri@microsoft.com>
2020-06-11 08:47:56 -0700

    Updates

---
+++ precache-manifest.f74eb3aae416002667187bdc5a37ef39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee8998881a699c496ba76364e3705e9",
+    "url": "/test-website/index.html"
+  },
+  {
+    "revision": "b3b8e0432df3ef245e3d",
+    "url": "/test-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23f544ed28f6f0d96ab5",
+    "url": "/test-website/static/js/2.9194525b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-website/static/js/2.9194525b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b8e0432df3ef245e3d",
+    "url": "/test-website/static/js/main.752dd9f1.chunk.js"
+  },
+  {
+    "revision": "8d4adef370bedb0e22df",
+    "url": "/test-website/static/js/runtime-main.c119014c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
