c26c471ad2dc7e19689c9ebfa670581c7422629c
SamHannat <shann053@uottawa.ca>
2020-06-15 14:33:49 -0400

    Added color picker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//import * as paintBucket from "paintBucket.js"; 
+
 // Whatever tool you implement will extend this class
 class Tool extends React.Component {
   constructor(props) {
@@ -20,22 +20,10 @@ class Tool extends React.Component {
   }
 }
 
-// Example Functions 
-function dothing(canvas, coordinates) {
-  var ctx = canvas.getContext("2d");
-  let rect = canvas.getBoundingClientRect();
-  var x = coordinates[0]; var y = coordinates[1];
-  let imageData = ctx.getImageData(x - rect.left, y - rect.top, 1,1).data;
-  console.log(imageData);
-  return canvas; 
-}
-
 function donothing(canvas, coordinates) {
   console.log(coordinates);
   return canvas; 
 }
-//
-//
 
 // How we interact with the canvas 
 class CanvasComponent extends React.Component {
@@ -45,7 +33,6 @@ class CanvasComponent extends React.Component {
   this.fileInput = React.createRef(); 
  }
  getInput(event) {
-   
    event.preventDefault();
    var image = new Image();
    image.src = URL.createObjectURL(this.fileInput.current.files[0]);
@@ -62,25 +49,16 @@ class CanvasComponent extends React.Component {
   ctx.drawImage(image,0,0);
  }
 
+  // For a function to be able to behave like an onCanvasClick or an onCanvasHover
+  // it needs to take the canvas and the current mouse coordinates as a parameter. 
   handleClick = (event) => {
-    // should take: complete canvas, current mouse coordinates
-    // should return: modified canvas
     var coordinates = [event.clientX, event.clientY];
-    
-    //const replace = window.current.props.onCanvasClick(this.refs.canvas,coordinates);
-    var ctx = this.refs.canvas.getContext("2d");
-    var imageData = ctx.getImageData(0, 0, this.refs.canvas.width, this.refs.canvas.height);
-    const replace = window.current.props.onCanvasClick(this.refs.canvas, coordinates);
-    this.refs.canvas.getContext("2d").drawImage(replace, 0,0);
+    window.current.props.onCanvasClick(this.refs.canvas, coordinates);
   }
 
   handleHover = (event) => {
-    // should take: complete canvas, current mouse coordinates
-    // should return: modified canvas
     var coordinates = [event.clientX, event.clientY];
-    
-    const replace = window.current.props.onCanvasHover(this.refs.canvas,coordinates);
-    this.refs.canvas.getContext("2d").drawImage(replace, 0,0);
+    window.current.props.onCanvasHover(this.refs.canvas,coordinates);
   }
 
   render() {
@@ -99,17 +77,23 @@ function getRGBAFromCoordinate(coordinates, imageData) {
   return [redCoord, redCoord+1, redCoord+2,redCoord+3];
 }
 
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return [parseInt(result[1], 16),
+    parseInt(result[2], 16),
+    parseInt(result[3], 16), 255]
+}
+
 
 function paintBucketClick(canvas, coordinates) {
-  console.log(coordinates)
   var ctx = canvas.getContext("2d");
   let rect = canvas.getBoundingClientRect();
   
   const x = Math.round(coordinates[0] - rect.left); const y = Math.round(coordinates[1] -rect.top);
   const pixelToCompare = ctx.getImageData(x, y, 1,1).data;
   var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  const pixelToFill = [11,11,11,255];
-  const tolerance = 0.5; 
+  const pixelToFill = hexToRgb(document.getElementById("color").value);
+  const tolerance = 0.1; 
 
   let visited = {};
   let q = [[x,y]];
@@ -150,8 +134,8 @@ function paintBucketClick(canvas, coordinates) {
 
 
 // current tool that is in use
-window.current =  <Tool url="https://image.flaticon.com/icons/png/512/66/66246.png" text="paintbrush" onCanvasClick={donothing} onCanvasHover={donothing}/>;
-
+var paintBrush  =  <Tool url="https://image.flaticon.com/icons/png/512/66/66246.png" text="paintbrush" onCanvasClick={donothing} onCanvasHover={donothing}/>;
+window.current = paintBrush; 
 
 class App extends React.Component {
 
@@ -161,7 +145,8 @@ class App extends React.Component {
         <header className="App-header">
           
           <CanvasComponent /> 
-          {window.current}
+          <input type="color" id="color"></input>
+          {paintBrush}
           <Tool url="https://cdn4.iconfinder.com/data/icons/proglyphs-design/512/Paint_Bucket-512.png" text="doggie time" onCanvasClick={paintBucketClick} onCanvasHover={donothing} />
         </header>
       </div>
